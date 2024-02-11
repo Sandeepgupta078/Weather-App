541bcc9311a1a80d8e80669b9b71c492f7f1bcce
Sandeep Kumar Gupta <103142653+Sandeepgupta078@users.noreply.github.com>
2024-02-11 16:40:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
 
-//initially vairables need????
+
 
 let oldTab = userTab;
 const API_KEY = "671683d3d983f6cb05d6871a0c9f0ce8";
@@ -21,17 +21,16 @@ function switchTab(newTab) {
         oldTab.classList.add("current-tab");
 
         if(!searchForm.classList.contains("active")) {
-            //kya search form wala container is invisible, if yes then make it visible
+            // search form  container is invisible, if yes then make it visible
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active");
         }
         else {
-            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
+    
             searchForm.classList.remove("active");
             userInfoContainer.classList.remove("active");
-            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
-            //for coordinates, if we haved saved them there.
+           
             getfromSessionStorage();
         }
     }
@@ -51,7 +50,7 @@ searchTab.addEventListener("click", () => {
 function getfromSessionStorage() {
     const localCoordinates = sessionStorage.getItem("user-coordinates");
     if(!localCoordinates) {
-        //agar local coordinates nahi mile
+        // if fnot find 
         grantAccessContainer.classList.add("active");
     }
     else {
